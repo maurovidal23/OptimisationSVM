37af901abaeb8e5502b17bea8a710e314028d88f
maurovidal23 <maurovidalantepazo@gmail.com>
2023-11-01 20:42:13 +0100

    first commit

---
+++ comp_primal_dual_svm.jl
@@ -0,0 +1,99 @@
+import Pkg
+Pkg.add("JuMP")
+using JuMP
+Pkg.add("HiGHS")
+using HiGHS
+Pkg.add("Plots")
+using Plots
+Pkg.add("AmplNLWriter")
+using AmplNLWriter
+#definición matriz de salidas
+salidas=zeros(10,10)
+
+
+#función clasificación de los datos
+function entrenamiento(data,vect_hiperplano,term_ind_hiperplano)
+    nobs=length(data[:,1])
+    dim=length(data[1,:])
+    #comprobamos que tienen dimensiones compatible 
+    if length(vect_hiperplano)!=dim
+        print("Error en la dimensión de los datos")
+    end
+
+    #definimos la condición 
+    #si w^T*x^i-r>0 =>true
+    #caso contrario =>false
+    condicion(x)= transpose(vect_hiperplano)*x>0
+
+    #define el vector y con los 1,-1 de entrenamiento
+    y=zeros(nobs)
+    for i in 1:nobs
+        y[i]=ifelse.(condicion(data[i,:]),1,-1)
+    end
+
+    return y
+end
+
+
+i=0;
+j=0;
+for dim in 1:5:50
+    #para una dimensión fijada, seleccionamos un hiperplano que nos permita clasificar los datos
+    #de entrenamiento que vamos a generar en el siguiente bucle
+    vect_hiperplano=rand(-50:50,dim) 
+    term_ind_hiperplano=rand(-50:50,1)
+    i=i+1
+    j=0
+    for nobs in 1:50:500
+        j=j+1
+        data=rand(-30:30,nobs,dim)
+
+        #Necesito función para clasificar y
+        y=entrenamiento(data,vect_hiperplano,term_ind_hiperplano)
+        
+        
+        #Modelo_primal
+        
+        #model_primal1=Model(HiGHS.Optimizer)
+        model_primal1=Model(() -> AmplNLWriter.Optimizer("bonmin"))
+
+
+        @variable(model_primal1,w[1:dim])
+        @variable(model_primal1,b)
+        @objective(model_primal1,Min,0.5*sum(w.^2))
+        @constraint(model_primal1,primal_c[i=1:nobs],y[i]*(b+sum(w.*data[i,:]))-1>=0)
+        #print(model_primal1)
+        time_primal=@elapsed optimize!(model_primal1)
+        
+        #Modelo_dual
+        
+        #model_dual1=Model(HiGHS.Optimizer)
+        model_dual1=Model(() -> AmplNLWriter.Optimizer("bonmin"))
+        @variable(model_dual1,u[1:nobs]>=0)
+        @objective(model_dual1,Max,sum(u)-0.5*sum(kron(u,transpose(u)).*kron(y,transpose(y)).*(data*transpose(data))))
+        @constraint(model_dual1,sum(y.*u)==0)
+        #print(model_dual1)
+        time_dual=@elapsed optimize!(model_dual1)
+
+        print("Time primal:", time_primal, " Time dual:", time_dual)
+        if time_dual<time_primal
+            salidas[i,j]=1
+        end
+    end
+end
+
+# Encuentra las coordenadas de los puntos 0 y 1 en la matriz
+coordenadas_0 = findall(x -> x == 0, salidas)
+coordenadas_1 = findall(x -> x == 1, salidas)
+
+# Convierte las coordenadas de CartesianIndex a tuplas de Int
+coordenadas_0 = Tuple.(coordenadas_0)
+coordenadas_1 = Tuple.(coordenadas_1)
+
+# Crea un nuevo gráfico y agrega puntos azules y rojos
+scatter([c[2] for c in coordenadas_0], [c[1] for c in coordenadas_0], color=:blue, legend=false, aspect_ratio=1)
+scatter!([c[2] for c in coordenadas_1], [c[1] for c in coordenadas_1] , color=:red)
+
+
+# Muestra el gráfico
+display(plot)
---
+++ ejemplo_optimizacion.jl
@@ -0,0 +1,79 @@
+#EJEMPLO TUTORIAL
+import Pkg
+Pkg.add("JuMP")
+using JuMP
+Pkg.add("HiGHS")
+using HiGHS
+
+model=Model(HiGHS.Optimizer)
+
+@variable(model,x>=0)
+@variable(model,0<= y<=3)
+
+@objective(model,Min,12x+20y)
+@constraint(model,c1,6x+8y>=100)
+@constraint(model,c2,7x+12y>=120)
+print(model)
+optimize!(model)
+
+#EJMEPLO DE CLASE
+
+#Leemos el archivo SVM_simulado_dim_4_obs_15_semilla_5.dat
+
+nobs=15
+dim=4
+d=
+#[] columnas
+data=[30 30 6 -5
+-19 11 44 -26
+-42 -39 22 43
+22 -5 47 -29
+40 -34 49 -13
+-20 -39 1 12
+-3 -22 -10 -7
+-31 29 -10 -42
+-46 27 10 -48
+39 36 10 -43
+23 2 -32 12
+1 6 26 24
+18 -47 -33 24
+-6 36 -3 30
+24 -45 -43 36;]
+
+y=[1, 1,  -1 , -1 , -1,  -1  ,-1 , 1  ,1,  1 , 1 , -1,  -1 , -1,  -1]
+
+model_primal1=Model(HiGHS.Optimizer)
+
+@variable(model_primal1,w[1:dim])
+@variable(model_primal1,b)
+@objective(model_primal1,Min,0.5*sum(w.^2))
+@constraint(model_primal1,primal_c[i=1:nobs],y[i]*(b+sum(w.*data[i,:]))-1>=0)
+print(model_primal1)
+optimize!(model_primal1)
+for i in 1:dim
+print(value(w[i]), " ")
+end
+
+model_dual1=Model(HiGHS.Optimizer)
+@variable(model_dual1,u[1:nobs]>=0)
+@objective(model_dual1,Max,sum(u)-0.5*sum(kron(u,transpose(u)).*kron(y,transpose(y)).*(data*transpose(data))))
+@constraint(model_dual1,sum(y.*u)==0)
+print(model_dual1)
+optimize!(model_dual1)
+
+w=zeros(dim)
+for d in 1:dim
+    w_d=0
+    for i in 1:nobs
+       global  w_d=w_d+y[i]*value(u[i])*data[i,d]
+        print(w_d)
+    end
+    w[d]=w_d 
+    
+end
+
+
+print(mostrar_w_dual(nobs,dim,y,data))
+
+@time_opti
+
